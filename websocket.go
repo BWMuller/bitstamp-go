69f05e6f5b803fec336dcc1e04cffc8c755a3cc3
Bernhard Muller <bmuller@retrorabbit.co.za>
2018-03-24 13:14:25 +0200

    Added websocket data conversion to order book

--- websocket.go
+++ websocket.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"runtime"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -18,13 +19,77 @@ type WebSocket struct {
 	Errors chan error
 }
 
+type EventOrderBookData struct {
+	Time time.Time
+	Bids []OrderBookItem
+	Asks []OrderBookItem
+}
+
 type Event struct {
+	Time  time.Time
 	Event string      `json:"event"`
 	Data  interface{} `json:"data"`
 }
 
+func (e *Event) AsOrderBookData() (*EventOrderBookData, error) {
+	res := &EventOrderBookData{}
+	res.Time = e.Time
+	res.Bids = make([]OrderBookItem, 0)
+	res.Asks = make([]OrderBookItem, 0)
+
+	tmp := &struct {
+		Bids [][]string `json:"bids"`
+		Asks [][]string `json:"asks"`
+	}{}
+	err := json.Unmarshal([]byte(e.Data.(string)), tmp)
+	if err != nil {
+		return nil, err
+	}
+	for _, val := range tmp.Bids {
+		p, err := strconv.ParseFloat(val[0], 64)
+		if err != nil {
+			return nil, err
+		}
+		a, err := strconv.ParseFloat(val[1], 64)
+		if err != nil {
+			return nil, err
+		}
+
+		res.Bids = append(res.Bids, OrderBookItem{Price: p, Amount: a})
+	}
+	for _, val := range tmp.Asks {
+		p, err := strconv.ParseFloat(val[0], 64)
+		if err != nil {
+			return nil, err
+		}
+		a, err := strconv.ParseFloat(val[1], 64)
+		if err != nil {
+			return nil, err
+		}
+
+		res.Asks = append(res.Asks, OrderBookItem{Price: p, Amount: a})
+	}
+	return res, nil
+}
+
 func (s *WebSocket) Close() {
 	s.quit <- true
+}
+
+func (s *WebSocket) LiveTrades(currencyPair string) {
+	s.Subscribe(fmt.Sprintf("live_trades_%s", currencyPair))
+}
+
+func (s *WebSocket) LiveOrders(currencyPair string) {
+	s.Subscribe(fmt.Sprintf("live_orders_%s", currencyPair))
+}
+
+func (s *WebSocket) OrderBook(currencyPair string) {
+	s.Subscribe(fmt.Sprintf("order_book_%s", currencyPair))
+}
+
+func (s *WebSocket) DiffOrderBook(currencyPair string) {
+	s.Subscribe(fmt.Sprintf("diff_order_book_%s", currencyPair))
 }
 
 func (s *WebSocket) Subscribe(channel string) {
@@ -55,7 +120,7 @@ func (s *WebSocket) Pong() {
 	s.ws.WriteJSON(a)
 }
 
-func NewWebSocket(t time.Duration) (*WebSocket, error) {
+func (b Bitstamp) NewWebSocket(t time.Duration) (*WebSocket, error) {
 	var err error
 	s := &WebSocket{
 		quit:   make(chan bool, 1),
@@ -91,6 +156,7 @@ func NewWebSocket(t time.Duration) (*WebSocket, error) {
 					s.Errors <- err
 					continue
 				}
+				e.Time = time.Now()
 				s.Stream <- e
 			}
 		}
